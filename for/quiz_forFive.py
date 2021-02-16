221d52f77e7b9ca9c6812cd4480c1c107de402cc
YoungHaChoi <askillofgod159@naver.com>
2021-02-16 17:47:11 +0900

    commit

--- for/quiz_forFive.py
+++ for/quiz_forFive.py
@@ -1,18 +1,14 @@
-# 리스트 뒤집기
+# 함수없이 리스트 뒤집기
 numbers = [2, 4, 6, 8, 10, 12, 14]
 
-for i in range(6, -1, -1):
-    temp = numbers[i]
-    numbers[i] = numbers[i-1]
-    numbers[i-1] = temp
-
-
-
-
-
-
-
 # 리스트 뒤집기
-# 코드를 입력하세요.
+for left_index in range(int(len(numbers) / 2)):
+
+    right_index = len(numbers) - left_index - 1
+
+    temp = numbers[left_index]
+    numbers[left_index] = numbers[right_index]
+    numbers[right_index] = temp
+
 
 print("뒤집어진 리스트: " + str(numbers))
